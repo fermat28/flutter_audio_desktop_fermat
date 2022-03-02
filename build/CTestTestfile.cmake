90a73f8bc6df9eb6170d6949674ff3589973a0ad
fermat28 <82266737+fermat28@users.noreply.github.com>
2022-03-02 09:50:04 +0100

    delete_build_directory
    not important

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fermat/Documents/Flutter projects/flutter_audio_desktop-master
-# Build directory: /home/fermat/Documents/Flutter projects/flutter_audio_desktop-master/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

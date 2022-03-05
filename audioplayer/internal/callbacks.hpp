97a9eb6efc8e69a805c01752daded867e03f6b8a
fermat28 <82266737+fermat28@users.noreply.github.com>
2022-03-05 22:08:44 +0100

    Update callbacks.hpp

--- audioplayer/internal/callbacks.hpp
+++ audioplayer/internal/callbacks.hpp
@@ -9,13 +9,13 @@ void dataCallbackStream(ma_device* pDevice, void* pOutput, const void* pInput, m
 void dataCallbackWave(ma_device *pDevice, void *pOutput, const void *pInput, ma_uint32 frameCount) {
     ma_waveform *pSineWave;
     pSineWave = (ma_waveform *)pDevice->pUserData;
-    ma_waveform_read_pcm_frames(pSineWave, pOutput, frameCount);
+    ma_waveform_read_pcm_frames(pSineWave, pOutput, frameCount, NULL);
     (void)pInput;
 }
 
 void dataCallbackNoise(ma_device *pDevice, void *pOutput, const void *pInput, ma_uint32 frameCount) {
     ma_noise *pNoise;
     pNoise = (ma_noise *)pDevice->pUserData;
-    ma_noise_read_pcm_frames(pNoise, pOutput, frameCount);
+    ma_noise_read_pcm_frames(pNoise, pOutput, frameCount, NULL);
     (void)pInput;
 }

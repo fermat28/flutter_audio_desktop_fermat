c60571f8ab92e452532ae93bdecec3c145d529e8
fermat28 <82266737+fermat28@users.noreply.github.com>
2022-03-05 10:09:50 +0100

    Create generated_config.cmake

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/fermat/Documents/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/fermat/Documents/Flutter projects/flutter_audio_desktop" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/fermat/Documents/flutter"
+  "PROJECT_DIR=/home/fermat/Documents/Flutter projects/flutter_audio_desktop"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/fermat/Documents/Flutter projects/flutter_audio_desktop/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/fermat/Documents/Flutter projects/flutter_audio_desktop/lib/main.dart"
+)
